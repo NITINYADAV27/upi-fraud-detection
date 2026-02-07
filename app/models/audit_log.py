083cc4ada92276e49fdc1880c9f8de2242c02629
NITINYADAV27 <nitinbitu03@gmail.com>
2026-02-08 02:35:04 +0530

    Update audit_log.py

--- app/models/audit_log.py
+++ app/models/audit_log.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from datetime import datetime
+
 from app.core.database import Base
 
 
@@ -7,12 +8,18 @@ class AuditLog(Base):
     __tablename__ = "audit_logs"
 
     id = Column(Integer, primary_key=True, index=True)
+
     tx_id = Column(String, index=True)
-    decision = Column(String)
-    reason = Column(String)
     amount = Column(Float)
+
     sender_vpa = Column(String)
     receiver_vpa = Column(String)
+
+    risk_score = Column(Integer)      # ✅ ADD THIS
+    decision = Column(String)
+
+    reason = Column(String, nullable=True)
+
     timestamp = Column(DateTime, default=datetime.utcnow)
 
 
