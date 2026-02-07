97a45963a063821180457c98ce1f8f4d10347df3
NITINYADAV27 <nitinbitu03@gmail.com>
2026-02-08 03:00:18 +0530

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,26 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import DATABASE_URL
 
-# SQLite needs this
-connect_args = {}
-if DATABASE_URL.startswith("sqlite"):
-    connect_args = {"check_same_thread": False}
+from app.core.config import DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args=connect_args
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 🔥 THIS WAS MISSING EARLIER
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
@@ -28,3 +18,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
