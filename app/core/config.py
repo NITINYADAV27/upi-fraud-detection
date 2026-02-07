034a0ab8e5425b27b133cf5d8d367ef925635aa3
NITINYADAV27 <nitinbitu03@gmail.com>
2026-02-08 02:14:03 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,11 @@
 import os
 
-BASE_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../../..")
+# Redis (Upstash)
+REDIS_URL = os.getenv("REDIS_URL")
+
+# SQLite (local + Render safe)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./fraud_audit.db"
 )
-
-# Raw DB file path (for sqlite3)
-DB_PATH = os.path.join(BASE_DIR, "fraud_audit.db")
-
-# SQLAlchemy-compatible URL
-DATABASE_URL = f"sqlite:///{DB_PATH}"
 
