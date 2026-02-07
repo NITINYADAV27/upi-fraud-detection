74f70ae253a621302c4235ad9e9ae61c5e3f108d
NITINYADAV27 <nitinbitu03@gmail.com>
2026-02-08 01:17:54 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,10 @@
 import os
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))
-    )
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../..")
 )
 
 DB_PATH = os.path.join(BASE_DIR, "fraud_audit.db")
+
+# ✅ Alias for backward compatibility
+DATABASE_URL = DB_PATH
